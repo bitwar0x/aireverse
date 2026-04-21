03ba898fe749af3c528d1bbaf0dcad999348d811
root <root@VM-8MD8J6Q6.VM-8MD8J6Q6>
2026-04-21 20:42:37 +0800

    fix bug

--- install.sh
+++ install.sh
@@ -125,7 +125,7 @@ install_soga() {
 #            exit 1
 #        fi
         echo -e "开始安装 soga 最新版"
-        wget -N --no-check-certificate -O /usr/local/soga.tar.gz https://github.com/bitwar0x/soga/releases/latest/download/soga-linux-${arch}.tar.gz
+        wget -N --no-check-certificate -O /usr/local/soga.tar.gz https://github.com/bitwar0x/aireverse/releases/latest/download/soga-linux-${arch}.tar.gz
         if [[ $? -ne 0 ]]; then
             echo -e "${red}下载 soga 失败，请确保你的服务器能够下载 Github 的文件${plain}"
             exit 1
